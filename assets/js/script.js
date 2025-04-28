96025d095f658558d99a337222a37f248a2480dc
Ignacio <148559176+iarayae@users.noreply.github.com>
2025-04-28 17:09:46 +0000

    JQuery modificado segun escala de evaluación

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 // Esperar que el documento esté listo
 $(document).ready(function () {
 
-    // Inicializar tooltips
-    $('[data-bs-toggle="tooltip"]').tooltip();
-  
-    // Al hacer click en el botón "Enviar por correo"
-    $('#enviarCorreoSec').click(function () {
-      alert('El correo fue enviado correctamente...');
-    });
-  
+  // 1. Inicializar tooltips (Bootstrap)
+  $('[data-bs-toggle="tooltip"]').tooltip();
+
+  // 2. Evento click en el botón "Enviar por correo"
+  $('#enviarCorreoSec').click(function () {
+    alert('El correo fue enviado correctamente...');
   });
+
+  // 3. Evento dblclick en títulos "INGREDIENTES" y "PREPARACIÓN"
+  $('h3').on('dblclick', function () {
+      $(this).css('color', '#DC3545'); 
+  });
+
+  // 4. Evento click en títulos de cards para toggle de descripción
+  $('.card-title').click(function () {
+      $(this).siblings('.card-text').toggle();
+  });
+
+});
